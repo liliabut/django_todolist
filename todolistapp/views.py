c4da4446b65a9301862a7c758f53579c5d809ce0
Lilia Butenkova <lilia.butenkova@gmail.com>
2015-05-05 01:20:46 +0200

    Anpassung der URL Routing

--- todolistapp/views.py
+++ todolistapp/views.py
@@ -11,3 +11,8 @@ def index(request):
     })
     
     return HttpResponse(template.render(context))
+    
+def impressum(request):
+    template = loader.get_template('impressum.html')
+    
+    return HttpResponse(template.render())
